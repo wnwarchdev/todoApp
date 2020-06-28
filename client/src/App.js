088cbb8d7e3738320a5f2b3c0facbcf08b24b9ba
Wojtek Nowak <billnewmann@gmail.com>
2020-06-28 15:41:29 +0100

    Remove default files; Add base code and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+    
+        <header>
+          <h1>ToDoList.app</h1>
+        </header>
+    
+        <section className="tasks-section" id="tasks-section">
+          <h2>Tasks</h2>
+    
+          <ul className="tasks-section__list" id="tasks-list">
+            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
+            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+          </ul>
+    
+          <form id="add-task-form">
+            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
+            <button className="btn" type="submit">Add</button>
+          </form>
+    
+        </section>
+      </div>
+    );
+  };
+
+};
 
 export default App;
